b87dbd6e7884f25f14e0b02171aa1f611d57a6d8
devi <depriyap@microsoft.com>
2024-03-21 13:51:15 +0530

    Merge unrelated histories

--- environment/vmss/main.tf
+++ environment/vmss/main.tf
@@ -19,28 +19,30 @@ variable "resource_group_name" {}
 
 variable "resource_name" {}
 
+variable "vnet" {}
+
 variable "location" {}
 
 data "azurerm_resource_group" "example" {
   name = var.resource_group_name
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+data "azurerm_virtual_network" "example" {
+  name                = var.vnet
   resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
+  #location            = data.azurerm_resource_group.example.location
+  #address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "Vmss subnet"
   resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+  virtual_network_name = data.azurerm_virtual_network.example.name
+  address_prefixes     = ["10.6.3.0/24"]
 }
 
 resource "azurerm_windows_virtual_machine_scale_set" "example" {
-  name                 = "example-vmss"
+  name                 = "xmew1-dop-c-tstoem-d-vmss-001"
   resource_group_name  = data.azurerm_resource_group.example.name
   location             = data.azurerm_resource_group.example.location
   sku                  = "Standard_F2"
