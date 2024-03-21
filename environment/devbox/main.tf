676b38fd75a659d49d30d688b3e5e075a77a8cfd
devi <depriyap@microsoft.com>
2024-03-21 16:04:53 +0530

    Merge unrelated histories

--- environment/devbox/main.tf
+++ environment/devbox/main.tf
@@ -30,26 +30,26 @@ variable "devbox_tags" {
   }
 }
 
-variable "devbox_properties" {
-  description = "Properties of the DevBox definition"
-  type        = map(any)
-  default = {
-    hibernateSupport = "enabled"
-    imageReference_id = "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-s-stamp-d-rg-001/providers/Microsoft.Compute/galleries/xmew1dopsstampdcomputegallery001/images/DevopsPilot_TestImage"
-    osStorageType = "standard"
-    sku_capacity = 1
-    sku_family   = "standard"
-    sku_name     = "DS1_v2"
-    sku_size     = "Standard_DS1_v2"
-    sku_tier     = "Standard"
-  }
-}
-
-resource "azapi_resource" "devbox" {
-  type      = "Microsoft.DevCenter/devcenters/devboxdefinitions@2023-04-01"
-  name      = var.devbox_name
-  location  = var.devbox_location
-  tags      = var.devbox_tags
+resource "azapi_resource" "symbolicname" {
+  type = "Microsoft.DevCenter/devcenters/devboxdefinitions@2023-04-01"
+  name = var.devbox_name
+  location = var.devbox_location
   parent_id = "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-c-tstoem-d-rg-001/providers/Microsoft.DevCenter/devcenters/xmew1-dop-c-tstoem-d-dc"
-  body      = jsonencode({ properties = var.devbox_properties })
+  tags = var.devbox_tags
+  body = jsonencode({
+    properties = {
+      hibernateSupport = "enabled"
+      imageReference = {
+        id = "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-s-stamp-d-rg-001/providers/Microsoft.Compute/galleries/xmew1dopsstampdcomputegallery001/images/DevopsPilot_TestImage"
+      }
+      osStorageType = "standard"
+      sku = {
+        capacity = 1
+        family = "standard"
+        name = "DS1_v2"
+        size = "Standard_DS1_v2"
+        tier = "standard"
+      }
+    }
+  })
 }
