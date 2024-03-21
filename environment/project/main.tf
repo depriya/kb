bdd74c21c2326bb531c8e33762faa8294629ea91
devi <depriyap@microsoft.com>
2024-03-21 17:01:12 +0530

    Merge unrelated histories

--- environment/project/main.tf
+++ environment/project/main.tf
@@ -19,3 +19,20 @@ resource "azurerm_dev_center_project" "project" {
   name                = var.projectname
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "example-uai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_shared_image_gallery" "example" {
+  name                = "xmew1dopsstampdcomputegallery001"
+  location            =  "West Europe "
+  resource_group_name = "xmew1-dop-s-stamp-d-rg-001"
+}
+resource "azurerm_dev_center_gallery" "example" {
+  dev_center_id     = azurerm_dev_center.dc.id
+  shared_gallery_id = azurerm_shared_image_gallery.example.id
+  name              = "example"
+}
