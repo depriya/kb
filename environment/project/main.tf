23806b603a2b7a962a91a6dba00a222e899ef1f6
devi <depriyap@microsoft.com>
2024-03-21 18:09:25 +0530

    Merge unrelated histories

--- environment/project/main.tf
+++ environment/project/main.tf
@@ -35,5 +35,5 @@ resource "azurerm_shared_image_gallery" "example" {
 resource "azurerm_dev_center_gallery" "gallery" {
   dev_center_id     = azurerm_dev_center.dc.id
   shared_gallery_id = azurerm_shared_image_gallery.example.id
-  name              = "example"
+  name              = "xmew1dopsstampdcomputegallery001"
 }
