2bff3bb3c0028f36b9beec0af413872dd1e16d7d
devi <depriyap@microsoft.com>
2024-03-24 18:37:09 +0530

    Merge unrelated histories

--- environment/devbox/main.tf
+++ environment/devbox/main.tf
@@ -1,18 +1,14 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+    azapi = {
+      source = "Azure/azapi"
     }
   }
-
-  required_version = ">= 1.0.0"
 }
 
-provider "azurerm" {
-  features {}
 
-  skip_provider_registration = true
+provider "azapi" {
+    skip_provider_registration = true
 }
 
 # Data sources to fetch IDs of existing resources
