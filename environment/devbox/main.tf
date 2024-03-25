84d7557810f11f72964231eeb663f46fed236858
devi <depriyap@microsoft.com>
2024-03-25 23:15:15 +0530

    Merge unrelated histories

--- environment/devbox/main.tf
+++ environment/devbox/main.tf
@@ -14,33 +14,6 @@ variable OEM{
   description = "The OEM Value in three letters"
 }
 
-variable "Project" {
-   default = "xmew1-dop-c-${var.OEM}-d-project-001"
-}
-
-variable "devcenter" {
-    default = "xmew1-dop-c-${var.OEM}-d-dc"
-}
-
-variable "definition" {
-    default = "xmew1-dop-c-${var.OEM}-devboxdef"
-}
-
-variable "resourcegroup" {
-    default = "xmew1-dop-c-${var.OEM}-d-rg-001"
-}
-
-variable "attachednetwork" {
-    default = "xmew1-dop-c-${var.OEM}-ntwk-001"
-}
-
-variable "networkconnection"{
-    default = "xmew1-dop-c-${var.OEM}-ntwkcon-001"
-}
-
-variable "networkrg" {
-  default = "xmew1-dop-c-${var.OEM}-d-rg-networkconnection-001"
-}
 variable "location"{
   default = "westeurope"
 }
@@ -53,9 +26,6 @@ variable "image" {
   default = {
     win11-ent-base   = "microsoftwindowsdesktop_windows-ent-cpc_win11-21h2-ent-cpc-os"
   }
-}
-variable "pool" {
-    default = "xmew1-dop-c-${var.OEM}-pools-001"
 }
 
 variable "vnet" {
@@ -71,7 +41,7 @@ variable "gallery" {
 
 data "azapi_resource" "existing_rg" {
   type = "Microsoft.Resources/resourceGroups@2022-09-01"
-  name = var.resourcegroup
+  name = "xmew1-dop-c-${var.OEM}-d-rg-001"
 }
 
 data "azapi_resource" "existing_vnet" {
@@ -89,7 +59,7 @@ data "azapi_resource" "existing_subnet" {
 # Data sources to fetch IDs of existing resources
 data "azapi_resource" "existing_devcenter" {
   type = "Microsoft.DevCenter/devcenters@2023-04-01"
-  name = var.devcenter
+  name = "xmew1-dop-c-${var.OEM}-d-dc"
   parent_id = data.azapi_resource.existing_rg.id
 }
 
@@ -101,13 +71,13 @@ data "azapi_resource" "existing_gallery" {
 
 data "azapi_resource" "existing_project" {
   type = "Microsoft.DevCenter/projects@2023-04-01"
-  name = var.Project
+  name = "xmew1-dop-c-${var.OEM}-d-project-001"
   parent_id = data.azapi_resource.existing_rg.id
 }
 # Define devbox definitions
 resource "azapi_resource" "devbox_definition" {
   type = "Microsoft.DevCenter/devcenters/devboxdefinitions@2023-04-01"
-  name = var.definition
+  name = "xmew1-dop-c-${var.OEM}-devboxdef"
   parent_id = data.azapi_resource.existing_devcenter.id
   body = jsonencode({
     location = "${var.location}"
@@ -133,15 +103,15 @@ resource "azapi_resource" "devbox_definition" {
 # Define pools
 resource "azapi_resource" "pool" {
   type = "Microsoft.DevCenter/projects/pools@2023-04-01"
-  name = var.pool
+  name = "xmew1-dop-c-${var.OEM}-pools-001"
   parent_id = data.azapi_resource.existing_project.id
   body = jsonencode({
     location = "${var.location}"
     properties = {
-      devBoxDefinitionName =  "${var.definition}"
+      devBoxDefinitionName = "xmew1-dop-c-${var.OEM}-devboxdef"
       licenseType = "Windows_Client"
       localAdministrator = "Enabled"
-      networkConnectionName = "${var.attachednetwork}"
+      networkConnectionName = "xmew1-dop-c-${var.OEM}-ntwk-001"
       
     }
   })
@@ -151,7 +121,7 @@ resource "azapi_resource" "pool" {
 # Define attached network
 resource "azapi_resource" "networkConnection" {
   type = "Microsoft.DevCenter/networkConnections@2023-01-01-preview"
-  name = var.networkconnection
+  name = "xmew1-dop-c-${var.OEM}-ntwkcon-001"
   parent_id = data.azapi_resource.existing_rg.id
   body = jsonencode({
     location = "${var.location}"
@@ -159,14 +129,14 @@ resource "azapi_resource" "networkConnection" {
       domainJoinType = "AzureADJoin"
       subnetId = "${data.azapi_resource.existing_subnet.id}"
       //subnetId = "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-c-abc-d-rg-001/providers/Microsoft.Network/virtualNetworks/xmew1-dop-c-oem-vnet-001/subnets/OEMSubnet"
-      networkingResourceGroupName = "${var.networkrg}"
+      networkingResourceGroupName = "xmew1-dop-c-${var.OEM}-d-rg-networkconnection-001"
     }
   })
 }
 
 resource "azapi_resource" "attachedNetworks" {
   type = "Microsoft.DevCenter/devcenters/attachednetworks@2023-01-01-preview"
-  name = var.attachednetwork
+  name = "xmew1-dop-c-${var.OEM}-ntwk-001"
   parent_id = data.azapi_resource.existing_devcenter.id
   body = jsonencode({
     properties = {
