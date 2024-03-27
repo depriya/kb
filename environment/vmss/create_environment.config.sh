9fa96e5fe240697c0750e7e4e4e45f6d8aea45fb
Heindl, Alexander AVL/AT <Alexander.Heindl@avl.com>
2024-03-27 23:19:10 +0100

    xmew1-dop-c-rrr-d-project-001

--- environment/vmss/create_environment.config.sh
+++ environment/vmss/create_environment.config.sh
@@ -12,7 +12,7 @@ ENVIRONMENT_NAME="vmss"
 ENVIRONMENT_TYPE="Test"
 
 # The name of your Azure dev center project.
-DEV_CENTER_PROJECT_NAME="xmew1-dop-c-rrr-d-project-002"
+DEV_CENTER_PROJECT_NAME="xmew1-dop-c-rrr-d-project-001"
 
 # The name of your catalog.
 DEV_CENTER_CATALOG_NAME="catalog"
