ff42985afffa4851a5eec9ad4165781baae71b2b
devi <depriyap@microsoft.com>
2024-03-28 00:46:31 +0530

    Merge unrelated histories

--- environment/project/variables.tf
+++ environment/project/variables.tf
@@ -7,31 +7,31 @@ variable "project"{
 variable "resource_group_id" {
   type        = string
   description = "The ID of the resource group in which to create the Dev Center project."
-  default = "/subscriptions/${var.target_subscription_id}/resourceGroups/xmew1-dop-c-${var.OEM}-d-rg-001"
+  //default = "/subscriptions/${var.target_subscription_id}/resourceGroups/xmew1-dop-c-${var.OEM}-d-rg-001"
 }
 
 variable "location" {
   type        = string
   description = "The location/region in which to create the Dev Center project."
-  default = "west europe"
+  //default = "west europe"
 }
 
 variable "devcenter_id" {
   type        = string
   description = "The ID of the Dev Center project."
-  default = "/subscriptions/${var.target_subscription_id}/resourceGroups/xmew1-dop-c-${var.OEM}-d-rg-001/providers/Microsoft.DevCenter/devcenters/xmew1-dop-c-${var.OEM}-d-dc"
+  //default = "/subscriptions/${var.target_subscription_id}/resourceGroups/xmew1-dop-c-${var.OEM}-d-rg-001/providers/Microsoft.DevCenter/devcenters/xmew1-dop-c-${var.OEM}-d-dc"
 }
 
 variable "project_description" {
   type        = string
   description = "The description of the Dev Center project."
-  default = "The description of the Dev Center project."
+  //default = "The description of the Dev Center project."
 }
 
 variable "project_members" {
   type        = list(string)
   description = "The members of the Dev Center project."
-  default = [ "b9082dac-d369-4435-a4b9-9779f666c1e0" ]  //Alex's objectid
+  //default = [ "b9082dac-d369-4435-a4b9-9779f666c1e0" ]  //Alex's objectid
 }
 
 variable "environment_types" {
@@ -41,20 +41,20 @@ variable "environment_types" {
     target_subscription_id = string
   }))
   description = "The environment types to create on the Dev Center project."
-  default = {
-    "Test" = {
-      name                   = "Test"
-      description            = "Description for Test environment"
-      target_subscription_id = "db401b47-f622-4eb4-a99b-e0cebc0ebad4"
-    }
-  }
+#   //default = {
+#     "Test" = {
+#       name                   = "Test"
+#       description            = "Description for Test environment"
+#       target_subscription_id = "db401b47-f622-4eb4-a99b-e0cebc0ebad4"
+#     }
+#   }
 }
 
 
 variable "project_name" {
   type        = string
   description = "The name of the Dev Center project."
-  default = "xmew1-dop-c-${var.OEM}-p-${var.project}-001"
+  //default = "xmew1-dop-c-${var.OEM}-p-${var.project}-001"
 }
 
 
@@ -62,11 +62,11 @@ variable "project_name" {
 variable "environment_name" {
   type        = string
   description = "The name of the Dev Center project."
-  default = "Test"
+  //default = "Test"
 }
 
 variable "target_subscription_id" {
   type        = string
   description = "The subscription ID of the target subscription."
-  default = "db401b47-f622-4eb4-a99b-e0cebc0ebad4"
+  //default = "db401b47-f622-4eb4-a99b-e0cebc0ebad4"
 }
