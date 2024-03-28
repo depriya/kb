646112367cd443a5b84f588c3d0841b32e1f9e5f
devi <depriyap@microsoft.com>
2024-03-28 12:17:44 +0530

    Merge unrelated histories

--- environment/vmss/create_environment.config.sh
+++ environment/vmss/create_environment.config.sh
@@ -6,7 +6,7 @@
 DEV_CENTER_NAME="xmew1-dop-c-rrr-d-dc"
 
 # The name to use for the new environment to be created.
-ENVIRONMENT_NAME="adevmss"
+ENVIRONMENT_NAME="rrrvmss"
 
 # The environment type to use for this environment.
 ENVIRONMENT_TYPE="Test"
