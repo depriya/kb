d0fbf683fa4581390c6458f0aa8547b22689c700
devi <depriyap@microsoft.com>
2024-03-28 15:01:47 +0530

    modifying code

--- environment/project/main.tf
+++ environment/project/main.tf
@@ -39,7 +39,9 @@ resource "azapi_resource" "environment_type_definition" {
        creatorRoleAssignment = {
         
           roles = {
-             "[f0e04b27-58c5-49a7-b142-5cc5296a4261]": {}
+             "owner": {
+                "principalId": "f0e04b27-58c5-49a7-b142-5cc5296a4261"
+            }
         }
        }
       deploymentTargetId = "/subscriptions/${var.target_subscription_id}"
