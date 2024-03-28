854b9e68b1183d67a4f26ec46fd612d3f019d632
devi <depriyap@microsoft.com>
2024-03-28 13:49:46 +0530

    modifying code

--- environment/project/main.tf
+++ environment/project/main.tf
@@ -37,7 +37,9 @@ resource "azapi_resource" "environment_type_definition" {
   body = jsonencode({
     properties = {
        creatorRoleAssignment = {
-        roles = {"Owner" = ["f0e04b27-58c5-49a7-b142-5cc5296a4261"]}
+        roles = {
+          Owner = ["f0e04b27-58c5-49a7-b142-5cc5296a4261"]
+        }
        }
       deploymentTargetId = "/subscriptions/${var.target_subscription_id}"
       status             = "Enabled"
