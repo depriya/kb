574d4dbafa6bf0a44aaecae677ca0651c3602d15
devi-priya_avl <devi.priya@microsoft.com>
2024-04-02 00:36:17 +0530

    modifying code

--- environment/devbox/trigger.sh
+++ environment/devbox/trigger.sh
@@ -10,4 +10,4 @@ git config user.name "devi-priya_avl"
 TAG_NAME="trigger-workflow-$(date +%Y%m%d%H%M%S)"  # Add a timestamp to make the tag unique
 #TAG_NAME="trigger"  # Add a timestamp to make the tag unique
 git tag -a "$TAG_NAME" -m "Trigger GitHub Actions workflow"
-git push
+git push origin "$TAG_NAME"
