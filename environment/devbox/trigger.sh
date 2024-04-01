91f338ab0181e542c019bc31fedd741836719c49
devi <depriyap@microsoft.com>
2024-04-01 17:55:08 +0530

    modifying code

--- environment/devbox/trigger.sh
+++ environment/devbox/trigger.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Replace these variables with your GitHub repository details and the event type
-owner="dev-sdv-devopspilot-dev-msft"
-repo="avl-sdv/DevOpsPilot.SiL.Workflows"
+#owner="dev-sdv-devopspilot-dev-msft"
+#repo="avl-sdv/DevOpsPilot.SiL.Workflows"
 workflow_file="devbox.yml"  # Specify the YAML file you want to trigger
 event_type="trigger-terraform"
 
 # Create a repository dispatch event using Azure CLI
 az rest \
   --method post \
-  --uri "https://api.github.com/repos/$owner/$repo/dispatches" \
+  #--uri "https://api.github.com/repos/$owner/$repo/dispatches" \
+  --uri "https://api.github.com/repos/avl-sdv/DevOpsPilot.SiL.Workflows" \
   --header "Accept=application/vnd.github.everest-preview+json" \
   --body "{ \"event_type\": \"$event_type\", \"client_payload\": { \"workflow_file\": \"$workflow_file\" } }"
