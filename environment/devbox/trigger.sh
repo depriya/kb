9395196e78022ac6ced005f053e3c1a23b22027f
devi <depriyap@microsoft.com>
2024-04-01 18:22:59 +0530

    modifying code

--- environment/devbox/trigger.sh
+++ environment/devbox/trigger.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set your Git configuration
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config --global user.email "devi.priya@microsoft.com"
+git config --global user.name "devi-priya_avl"
 
 # Create and push a trigger tag
 git tag -a trigger-workflow -m "Trigger GitHub Actions workflow"
