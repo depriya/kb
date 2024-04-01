d0947faaf9b74e88db2c02b66dd6ea2b673e986a
devi-priya_avl <devi.priya@microsoft.com>
2024-04-02 00:13:12 +0530

    modifying code

--- environment/devbox/trigger.sh
+++ environment/devbox/trigger.sh
@@ -2,12 +2,12 @@
 
 
 # Set your Git configuration
-git config --global user.email "devi.priya@microsoft.com"
-git config --global user.name "devi-priya_avl"
+#git config --global user.email "devi.priya@microsoft.com"
+#git config --global user.name "devi-priya_avl"
 
 
 # Create and push a trigger tag
-#TAG_NAME="trigger-workflow-$(date +%Y%m%d%H%M%S)"  # Add a timestamp to make the tag unique
-TAG_NAME="triggernew"  # Add a timestamp to make the tag unique
+TAG_NAME="trigger-workflow-$(date +%Y%m%d%H%M%S)"  # Add a timestamp to make the tag unique
+#TAG_NAME="trigger"  # Add a timestamp to make the tag unique
 git tag -a "$TAG_NAME" -m "Trigger GitHub Actions workflow"
 git push origin "$TAG_NAME"
