4857483a57948a5f0a92d778bca2dcfe0002ea58
devi-priya_avl <devi.priya@microsoft.com>
2024-04-04 12:29:49 +0530

    modifying code

---
+++ environment/devbox/createdevbox.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright (C) Microsoft Corporation.
+
+# Exit immediately if a command fails
+set -e
+
+# Fail if an unset variable is used
+set -u
+
+# Enable tracing for debugging
+set -x
+
+# Change the current directory to the location of the script
+cd "$(dirname "$0")"
+
+
+#!/bin/bash
+
+# Copyright (C) Microsoft Corporation.
+customerOEMsuffix="avl" #comes from metamodel
+projectname="pj" #comes from metamodel
+project="xmew1-dop-c-${customerOEMsuffix}-p-${projectname}-001"
+DEV_CENTER_NAME="xmew1-dop-c-${var.customerOEMsuffix}-d-dc"
+Pool_name="xmew1-dop-c-${var.customerOEMsuffix}-pools-001"
+SUBID="db401b47-f622-4eb4-a99b-e0cebc0ebad4"
+MYOID="f0e04b27-58c5-49a7-b142-5cc5296a4261" 
+ # Assign admin access
+ az role assignment create --assignee $MYOID \
+ --role "DevCenter Project Admin" \
+ --scope "/subscriptions/$SUBID"
+  # Assign admin access
+ az role assignment create --assignee $MYOID \
+ --role "DevCenter Dev Box User" \
+ --scope "/subscriptions/$SUBID"
+az devcenter dev dev-box create --pool-name $Pool_name --name "DevBoxavl" --dev-center-name $DEV_CENTER_NAME --project-name $project --user-id "me"
+# # Disable tracing
+ set +x
