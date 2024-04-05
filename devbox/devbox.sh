4fb2e573c6c21deea839967da7a80fe16bcb4b07
devi-priya_avl <devi.priya@microsoft.com>
2024-04-05 12:09:33 +0530

    modifying code

--- devbox/devbox.sh
+++ devbox/devbox.sh
@@ -3,11 +3,10 @@
 # Set variables
 customerOEMsuffix="avl"
 location="westeurope"
-imagename="imagedef"
 image="/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-c-avl-d-rg-001/providers/Microsoft.DevCenter/devcenters/xmew1-dop-c-avl-d-dc/galleries/default/images/microsoftwindowsdesktop_windows-ent-cpc_win11-21h2-ent-cpc-os"
-vnet="vnetname"
 projectname="pjdb"
-subnet="OEMSubnet"
+project="xmew1-dop-c-${customerOEMsuffix}-p-${projectname}-001"
+DEV_CENTER_NAME="xmew1-dop-c-${customerOEMsuffix}-d-dc"
 
 echo "Installing the devcenter extension..."
 az extension add --name devcenter --upgrade || handle_error "Failed to install the devcenter extension."
@@ -15,7 +14,7 @@ echo "Extension installation complete!"
 
 # Create devbox definition
 az devcenter admin devbox-definition create \
-    --dev-center "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-c-avl-d-rg-001/providers/Microsoft.DevCenter/devcenters/xmew1-dop-c-avl-d-dc" \
+    --dev-center $DEV_CENTER_NAME \
     --devbox-definition-name "xmew1-dop-c-${customerOEMsuffix}-devboxdef" \
     --image-reference '{"id": "'"$image"'"}' \
     --os-storage-type "ssd_256gb" \
@@ -36,7 +35,7 @@ az devcenter admin network-connection create \
 # Create attached network
 az devcenter admin attached-network create \
     --attached-network-connection-name "xmew1-dop-c-${customerOEMsuffix}-ntwk-001" \
-    --dev-center "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-c-avl-d-rg-001/providers/Microsoft.DevCenter/devcenters/xmew1-dop-c-avl-d-dc" \
+    --dev-center $DEV_CENTER_NAME \
     --network-connection-id "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-c-avl-d-rg-001/providers/Microsoft.DevCenter/networkConnections/xmew1-dop-c-avl-d-ntwkcon-001" \
     --resource-group "xmew1-dop-c-${customerOEMsuffix}-d-rg-001"
 
@@ -45,7 +44,7 @@ az devcenter admin pool create \
     --devbox-definition-name "xmew1-dop-c-${customerOEMsuffix}-devboxdef" \
     --local-administrator "Enabled" \
     --name "xmew1-dop-c-${customerOEMsuffix}-pools-001" \
-    --project "/subscriptions/db401b47-f622-4eb4-a99b-e0cebc0ebad4/resourceGroups/xmew1-dop-c-avl-d-rg-001/providers/Microsoft.DevCenter/projects/xmew1-dop-c-avl-p-pjdb-001" \
+    --project $project \
     --resource-group "xmew1-dop-c-${customerOEMsuffix}-d-rg-001" \
     --location "$location" \
     --network-connection-name "xmew1-dop-c-${customerOEMsuffix}-ntwk-001"
