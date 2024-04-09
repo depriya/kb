c1e8694c882fbd157ead0c3c8a0a8eef245f202f
devi-priya_avl <devi.priya@microsoft.com>
2024-04-09 11:01:02 +0530

    modifying code

--- Archive/avl_devops_create_ade.sh
+++ Archive/avl_devops_create_ade.sh
@@ -8,43 +8,41 @@ set -e
 # Fail if an unset variable is used
 set -u
 
-#source $(dirname $0)/symphony_stage_script_provider.sh
+source $(dirname $0)/symphony_stage_script_provider.sh
 
 #region Declare Constants
+COMPUTE_GALLERY="Computegallery"             #TODO: Discuss. Should this come from the config?
 DEV_CENTER_CATALOG_NAME="catalog"            #TODO: Discuss. Should this come from the config?
 ENVIRONMENT_DEFINITION_NAME="vmss"           #TODO: Discuss. Should this come from the config?
 ENVIRONMENT_TYPE="sandbox"                   #TODO: Discuss. Should this come from the config?
-#PARAMETERS_FILE="existing.json"              #TODO: This should be removed, as the file is no longer used.
-admin_username="admin"                       #TODO: Discuss and change, if needed
+ADMIN_USER="avluser"                       #TODO: Discuss and change, if needed
 admin_password="Password@123"                #TODO: Should get this from the Key Vault or Generate a new password and save it in the Key Vault
-SUBID="db401b47-f622-4eb4-a99b-e0cebc0ebad4" #TODO: Discuss. Should this come from the config?
 MYOID="7cc6c11b-ad9c-43cc-a7d5-2a0a0e4f3648" #TODO: Discuss. How to get this value? As of now hardcoded objectid of AKS
 #endregion Declare Constants
 
-# #region Getting config from metamodel config yaml
-# configEncoded="{{ parameters.input_parameter_to_script }}"
-# config=$(echo $configEncoded | base64 -d)
-# #endregion Getting config from metamodel config yaml
-
-# #region parameters - get from config
-# customer_OEM_suffix=$(echo $config | jq -r '.customer_OEM_suffix')
-# project_name=$(echo $config | jq -r '.project_name')
-# environment_stage_short=$(echo $config | jq -r '.environment_stage_short')
-# project_description=$(echo $config | jq -r '.project_description')
-# #endregion parameters - get from config
+#region Getting config from metamodel config yaml
+configEncoded="{{ parameters.input_parameter_to_script }}"
+config=$(echo $configEncoded | base64 -d)
+#endregion Getting config from metamodel config yaml
 
 #region parameters - get from config
-customer_OEM_suffix="avl"
-project_name="mypj"
-environment_stage_short="d"
-project_description="firstproject"
+resource_name_primary_prefix=$(echo $config | jq -r '.resource_name_primary_prefix')
+resource_name_secondary_prefix=$(echo $config | jq -r '.resource_name_secondary_prefix')
+resource_name_shared_short=$(echo $config | jq -r '.resource_name_shared_short')
+resource_name_customer_short=$(echo $config | jq -r '.resource_name_customer_short')
+customer_OEM_suffix=$(echo $config | jq -r '.customer_OEM_suffix')
+project_name=$(echo $config | jq -r '.project_name')
+environment_stage_short=$(echo $config | jq -r '.environment_stage_short')
+project_description=$(echo $config | jq -r '.project_description')
+subscription_id=$(echo $config | jq -r '.subscription_id')
+#endregion parameters - get from config
 
 #region Set the variables
-#TODO: Discuss. Can the prefix and suffixes be fetched from the config? How to handle the index?
-RESOURCE_GROUP="xmew1-dop-c-${customer_OEM_suffix}-${environment_stage_short}-rg-001"
-DEV_CENTER_NAME="xmew1-dop-c-${customer_OEM_suffix}-${environment_stage_short}-dc"
-ENVIRONMENT_NAME="xmew1-dop-c-${customer_OEM_suffix}-p-${project_name}-vmss-001"
-project="xmew1-dop-c-${customer_OEM_suffix}-p-${project_name}-001".
+SHARED_RESOURCE_GROUP="${resource_name_primary_prefix}-${resource_name_secondary_prefix}-${resource_name_shared_short}-stamp-${environment_stage_short}-rg-001"
+RESOURCE_GROUP="${resource_name_primary_prefix}-${resource_name_secondary_prefix}-${resource_name_customer_short}-${customer_OEM_suffix}-${environment_stage_short}-rg-001"
+DEV_CENTER_NAME="${resource_name_primary_prefix}-${resource_name_secondary_prefix}-${resource_name_customer_short}-${customer_OEM_suffix}-${environment_stage_short}-dc"
+ENVIRONMENT_NAME="${resource_name_primary_prefix}-${resource_name_secondary_prefix}-${resource_name_customer_short}-${customer_OEM_suffix}-p-${project_name}-vmss-001"
+PROJECT="${resource_name_primary_prefix}-${resource_name_secondary_prefix}-${resource_name_customer_short}-${customer_OEM_suffix}-p-${project_name}-001".
 #endregion Set the variables
 
 #region Install Azure Dev Center extension
@@ -54,6 +52,7 @@ commandInstallDevCenterExt_output=""
 commandInstallDevCenterExt_status=0
 execute_command_with_status_code "$commandInstallDevCenterExt" commandInstallDevCenterExt_output commandInstallDevCenterExt_status
 if [ $commandInstallDevCenterExt_status -ne 0 ]; then
+  echo "Failed to install Azure Dev Center extension."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -64,11 +63,12 @@ az configure --defaults group=
 
 #region Get Role Id for the Subscription Owner
 echo "Getting Role Id for the Subscription Owner"
-commandGetOwnerRoleId="az role definition list -n \"Owner\" --scope \"/subscriptions/$SUBID\" --query [].name -o tsv"
+commandGetOwnerRoleId="az role definition list -n \"Owner\" --scope \"/subscriptions/$subscription_id\" --query [].name -o tsv"
 commandGetOwnerRoleId_output=""
 commandGetOwnerRoleId_status=0
 execute_command_with_status_code "$commandGetOwnerRoleId" commandGetOwnerRoleId_output commandGetOwnerRoleId_status
 if [ $commandGetOwnerRoleId_status -ne 0 ]; then
+  echo "Failed to get Role Id for the Subscription Owner."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -84,6 +84,7 @@ commandGetDevCenterId_output=""
 commandGetDevCenterId_status=0
 execute_command_with_status_code "$commandGetDevCenterId" commandGetDevCenterId_output commandGetDevCenterId_status
 if [ $commandGetDevCenterId_status -ne 0 ]; then
+  echo "Failed to get Azure Dev Center Resource ID."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -95,6 +96,7 @@ commandGetDevCenterObjId_output=""
 commandGetDevCenterObjId_status=0
 execute_command_with_status_code "$commandGetDevCenterObjId" commandGetDevCenterObjId_output commandGetDevCenterObjId_status
 if [ $commandGetDevCenterObjId_status -ne 0 ]; then
+  echo "Failed to get Azure Dev Center Object ID."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -102,13 +104,13 @@ echo "Got Azure Dev Center Object ID: $commandGetDevCenterObjId_output"
 #endregion Get Dev Center ID, Object ID
 
 #region Get Managed Identity ID, Object ID
-#TODO: Check why this resource group and name are hardcoded?
 echo "Getting Managed Identity Resource ID"
-commandGetManagedIdentityId="az identity show --name \"Computegalleryid\" --resource-group \"xmew1-dop-s-stamp-d-rg-001\" --query id -o tsv"
+commandGetManagedIdentityId="az identity show --name \"$COMPUTE_GALLERY\" --resource-group \"$SHARED_RESOURCE_GROUP\" --query id -o tsv"
 commandGetManagedIdentityId_output=""
 commandGetManagedIdentityId_status=0
 execute_command_with_status_code "$commandGetManagedIdentityId" commandGetManagedIdentityId_output commandGetManagedIdentityId_status
 if [ $commandGetManagedIdentityId_status -ne 0 ]; then
+  echo "Failed to get Managed Identity Resource ID."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -120,6 +122,7 @@ commandGetManagedIdentityObjId_output=""
 commandGetManagedIdentityObjId_status=0
 execute_command_with_status_code "$commandGetManagedIdentityObjId" commandGetManagedIdentityObjId_output commandGetManagedIdentityObjId_status
 if [ $commandGetManagedIdentityObjId_status -ne 0 ]; then
+  echo "Failed to get Managed Identity Object ID."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -128,35 +131,38 @@ echo "Got Managed Identity Object ID: $commandGetManagedIdentityObjId_output"
 
 #region Create Project in Dev Center
 echo "Creating Project in Azure Dev Center"
-commandCreateProject="az devcenter admin project create -n \"$project\" --description \"$project_description\" --dev-center-id \"$commandGetDevCenterId_output\""
+commandCreateProject="az devcenter admin project create -n \"$PROJECT\" --description \"$project_description\" --dev-center-id \"$commandGetDevCenterId_output\""
 commandCreateProject_output=""
 commandCreateProject_status=0
 execute_command_with_status_code "$commandCreateProject" commandCreateProject_output commandCreateProject_status
 if [ $commandCreateProject_status -ne 0 ]; then
+  echo "Failed to create Project in Azure Dev Center."
   echo_output_dictionary_to_output_file
   exit 0
 fi
-echo "Created Project: $project"
+echo "Created Project: $PROJECT"
 #endregion Create Project in Dev Center
 
 #region Assign Owner role to the Dev Center and Managed Identity, on the subscription
 echo "Assigning Owner role to the Dev Center Object Id on the subscription"
-commandCreateDevCenterRole="az role assignment create --role \"Owner\" --assignee-object-id \"$commandGetDevCenterObjId_output\" --scope \"/subscriptions/$SUBID\""
+commandCreateDevCenterRole="az role assignment create --role \"Owner\" --assignee-object-id \"$commandGetDevCenterObjId_output\" --scope \"/subscriptions/$subscription_id\""
 commandCreateDevCenterRole_output=""
 commandCreateDevCenterRole_status=0
 execute_command_with_status_code "$commandCreateDevCenterRole" commandCreateDevCenterRole_output commandCreateDevCenterRole_status
 if [ $commandCreateDevCenterRole_status -ne 0 ]; then
+  echo "Failed to assign Owner role to the Dev Center Object Id on the subscription."
   echo_output_dictionary_to_output_file
   exit 0
 fi
 echo "Assigned Owner role to the Dev Center Object Id on the subscription"
 
 echo "Assigning Owner role to the Managed Identity Object Id on the subscription"
-commandCreateManagedIdRole="az role assignment create --role \"Owner\" --assignee-object-id \"$commandGetManagedIdentityObjId_output\" --scope \"/subscriptions/$SUBID\""
+commandCreateManagedIdRole="az role assignment create --role \"Owner\" --assignee-object-id \"$commandGetManagedIdentityObjId_output\" --scope \"/subscriptions/$subscription_id\""
 commandCreateManagedIdRole_output=""
 commandCreateManagedIdRole_status=0
 execute_command_with_status_code "$commandCreateManagedIdRole" commandCreateManagedIdRole_output commandCreateManagedIdRole_status
 if [ $commandCreateManagedIdRole_status -ne 0 ]; then
+  echo "Failed to assign Owner role to the Managed Identity Object Id on the subscription."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -165,22 +171,24 @@ echo "Assigned Owner role to the Managed Identity Object Id on the subscription"
 
 #region Create Environment Type for the Project, assign Contributor role on the subscription
 echo "Creating Project Environment Type"
-commandCreateProjectEnvType="az devcenter admin project-environment-type create -n \"$ENVIRONMENT_TYPE\" --project \"$project\" --identity-type \"SystemAssigned\" --roles \"{\"${commandGetOwnerRoleId_output}\":{}}\" --deployment-target-id \"/subscriptions/${SUBID}\" --status Enabled --query 'identity.principalId' --output tsv"
+commandCreateProjectEnvType="az devcenter admin project-environment-type create -n \"$ENVIRONMENT_TYPE\" --project \"$PROJECT\" --identity-type \"SystemAssigned\" --roles \"{\"${commandGetOwnerRoleId_output}\":{}}\" --deployment-target-id \"/subscriptions/${subscription_id}\" --status Enabled --query 'identity.principalId' --output tsv"
 commandCreateProjectEnvType_output=""
 commandCreateProjectEnvType_status=0
 execute_command_with_status_code "$commandCreateProjectEnvType" commandCreateProjectEnvType_output commandCreateProjectEnvType_status
 if [ $commandCreateProjectEnvType_status -ne 0 ]; then
+  echo "Failed to create Project Environment Type."
   echo_output_dictionary_to_output_file
   exit 0
 fi
 echo "Created Project Environment Type with Object ID: $commandCreateProjectEnvType_output"
 
 echo "Assigning Contributor role to the Project Environment Type Object Id on the subscription"
-commandCreateContributorRole=""az role assignment create --role \"Contributor\" --assignee-object-id $commandCreateProjectEnvType_output --scope \"/subscriptions/$SUBID\"""
+commandCreateContributorRole="az role assignment create --role \"Contributor\" --assignee-object-id $commandCreateProjectEnvType_output --scope \"/subscriptions/$subscription_id\""
 commandCreateContributorRole_output=""
 commandCreateContributorRole_status=0
 execute_command_with_status_code "$commandCreateContributorRole" commandCreateContributorRole_output commandCreateContributorRole_status
 if [ $commandCreateContributorRole_status -ne 0 ]; then
+  echo "Failed to assign Contributor role to the Project Environment Type Object Id on the subscription."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -193,22 +201,24 @@ echo "Assigned Contributor role to the Project Environment Type Object Id on the
 
 #region Assign Dev Center Project Admin role, Deployment Environments User to MYOID
 echo "Assigning Dev Center Project Admin role, Deployment Environments User to $MYOID"
-commandAssignProjRole="az role assignment create --assignee \"$MYOID\" --role \"DevCenter Project Admin\" --scope \"/subscriptions/$SUBID\""
+commandAssignProjRole="az role assignment create --assignee \"$MYOID\" --role \"DevCenter Project Admin\" --scope \"/subscriptions/$subscription_id\""
 commandAssignProjRole_output=""
 commandAssignProjRole_status=0
 execute_command_with_status_code "$commandAssignProjRole" commandAssignProjRole_output commandAssignProjRole_status
 if [ $commandAssignProjRole_status -ne 0 ]; then
+  echo "Failed to assign Dev Center Project Admin role to $MYOID."
   echo_output_dictionary_to_output_file
   exit 0
 fi
 echo "Assigned Dev Center Project Admin role to $MYOID"
 
 echo "Assigning Deployment Environments User role to $MYOID"
-commandAssignEnvUserRole="az role assignment create --assignee \"$MYOID\" --role \"Deployment Environments User\" --scope \"/subscriptions/$SUBID\""
+commandAssignEnvUserRole="az role assignment create --assignee \"$MYOID\" --role \"Deployment Environments User\" --scope \"/subscriptions/$subscription_id\""
 commandAssignEnvUserRole_output=""
 commandAssignEnvUserRole_status=0
 execute_command_with_status_code "$commandAssignEnvUserRole" commandAssignEnvUserRole_output commandAssignEnvUserRole_status
 if [ $commandAssignEnvUserRole_status -ne 0 ]; then
+  echo "Failed to assign Deployment Environments User role to $MYOID."
   echo_output_dictionary_to_output_file
   exit 0
 fi
@@ -217,10 +227,15 @@ echo "Assigned Deployment Environments User role to $MYOID"
 
 #region Create Dev Environment
 echo "Creating Dev Environment"
-commandCreateDevCenter="az devcenter dev environment create --environment-name \"$ENVIRONMENT_NAME\" --environment-type \"$ENVIRONMENT_TYPE\" --dev-center-name \"$DEV_CENTER_NAME\" --project-name \"$project\" --catalog-name \"$DEV_CENTER_CATALOG_NAME\" --environment-definition-name \"$ENVIRONMENT_DEFINITION_NAME\" --parameters '{"customerOEMsuffix":"${customer_OEM_suffix}","admin_username":"${admin_username}","admin_password":"${admin_password}","environmentStage":"${environment_stage_short}","projectname":"${project}"}'"
+commandCreateDevCenter="az devcenter dev environment create --environment-name \"$ENVIRONMENT_NAME\" --environment-type \"$ENVIRONMENT_TYPE\" --dev-center-name \"$DEV_CENTER_NAME\" --project-name \"$PROJECT\" --catalog-name \"$DEV_CENTER_CATALOG_NAME\" --environment-definition-name \"$ENVIRONMENT_DEFINITION_NAME\" --parameters '{"customerOEMsuffix":"${customer_OEM_suffix}","admin_username":"${ADMIN_USER}","admin_password":"${admin_password}","environmentStage":"${environment_stage_short}","projectname":"${PROJECT}"}'"
 commandCreateDevCenter_output=""
 commandCreateDevCenter_status=0
 execute_command_with_status_code "$commandCreateDevCenter" commandCreateDevCenter_output commandCreateDevCenter_status
+if [ $commandCreateDevCenter_status -ne 0 ]; then
+  echo "Failed to create Dev Environment."
+  echo_output_dictionary_to_output_file
+  exit 0
+fi
 echo "Created Dev Environment: $ENVIRONMENT_NAME"
 #endregion Create Dev Environment
 
