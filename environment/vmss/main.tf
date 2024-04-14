52eac40cd8941fd0581219903b464387a7449a06
devi-priya_avl <devi.priya@microsoft.com>
2024-04-14 23:46:28 +0530

    modifying code

--- environment/vmss/main.tf
+++ environment/vmss/main.tf
@@ -91,12 +91,7 @@ data "azurerm_shared_image" "all" {
 locals {
   filtered_images = [for image in values(data.azurerm_shared_image.all) : image if regex("sms", image.name)]
   //filtered_images = [for image in values(data.azurerm_shared_image.all) : image if contains("${image.name}", "sms")]
-
-  //filtered_images = [for image in values(data.azurerm_shared_image.all) : image if contains(tostring(image.name), "sms")]
 }
-
-
-
 //locals {
   //filtered_images = [for name, image in data.azurerm_shared_image.all : image if contains(name, "sms")]
 //}
