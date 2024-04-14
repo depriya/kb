908b8752a789f52919046e7d77cdee27740cde97
devi-priya_avl <devi.priya@microsoft.com>
2024-04-14 23:41:49 +0530

    modifying code

--- environment/vmss/main.tf
+++ environment/vmss/main.tf
@@ -88,17 +88,16 @@ data "azurerm_shared_image" "all" {
 }
 
 
-//locals {
+locals {
   //filtered_images = [for image in values(data.azurerm_shared_image.all) : image if regex("sms", image.name)]
-//}
+  filtered_images = [for image in values(data.azurerm_shared_image.all) : image if contains(tostring(image.name), "sms")]
+}
+
+
 
 //locals {
   //filtered_images = [for name, image in data.azurerm_shared_image.all : image if contains(name, "sms")]
 //}
-
-locals {
-  filtered_images = [for name, image in data.azurerm_shared_image.all : image if contains(name, "sms")]
-}
 
 resource "random_password" "vmss_password" {
   length  = var.admin_password_length
