f813d76d462b9109624a4553a218a0ea2790ade9
devi-priya_avl <devi.priya@microsoft.com>
2024-04-15 23:10:11 +0530

    modifying code

--- environment/vmss/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "xmew1-dop-c-avl-d-rg-001"
-    storage_account_name = "xmew1dopcavldst"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
