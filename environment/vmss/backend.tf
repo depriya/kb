7cdcc09d3a069e41e9ad255e1f3546eb797bb76b
devi-priya_avl <devi.priya@microsoft.com>
2024-04-15 18:42:02 +0530

    modifying code

--- environment/vmss/backend.tf
+++ environment/vmss/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "xmew1-dop-c-${var.customerOEMsuffix}-${var.environmentStage}-rg-001"
-    storage_account_name = "xmew1dopc${var.customerOEMsuffix}${var.environmentStage}st"
+    resource_group_name  = data.azurerm_storage_account.tfstate.resource_group_name
+    storage_account_name = data.azurerm_storage_account.tfstate.name
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
--- environment/vmss/main.tf
+++ environment/vmss/main.tf
@@ -17,7 +17,34 @@ provider "azurerm" {
 
 provider "random" {}
 
+variable "customerOEMsuffix" {
+  default = "avl"
+}
 
+variable "projectname" {
+  default = "pj9"
+}
+
+variable "admin_username" {
+  default = "avluser"
+}
+
+variable "admin_password_length" {
+  description = "The length of the generated admin password"
+  default     = 20
+}
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "environmentStage" {
+  default = "d"
+}
+
+variable "gallery_name" {
+  default = "xmew1dopsstampdcomputegallery001"
+}
 
 
 data "azurerm_storage_account" "tfstate" {
