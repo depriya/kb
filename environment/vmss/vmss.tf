e2a50d908bedb965d5164403ca505a9ed912442b
devi-priya_avl <devi.priya@microsoft.com>
2024-04-17 14:48:38 +0530

    modifying code

--- environment/vmss/vmss.tf
+++ environment/vmss/vmss.tf
@@ -9,16 +9,16 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "xmew1-dop-c-avl-d-rg-001"
-    storage_account_name = "xmew1dopcavldst"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-    client_id = "13b095e6-55df-4d76-9c6d-b59404e4d506"
-    tenant_id = "b4bc7e59-9a34-4622-ab54-d7a1a680f47a"
-    subscription_id = "db401b47-f622-4eb4-a99b-e0cebc0ebad4"
-  }
+  # #backend "azurerm" {
+  #   resource_group_name  = "xmew1-dop-c-avl-d-rg-001"
+  #   storage_account_name = "xmew1dopcavldst"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  #   use_oidc             = true
+  #   client_id = "13b095e6-55df-4d76-9c6d-b59404e4d506"
+  #   tenant_id = "b4bc7e59-9a34-4622-ab54-d7a1a680f47a"
+  #   subscription_id = "db401b47-f622-4eb4-a99b-e0cebc0ebad4"
+  # }
 }
 
 
