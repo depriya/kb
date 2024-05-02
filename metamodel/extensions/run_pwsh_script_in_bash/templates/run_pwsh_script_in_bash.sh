f7ff6287ee1e124c759f7d18489c79587972f9bb
Suresh Patamsetty <65555590+sureshpatamsetty96@users.noreply.github.com>
2024-05-02 10:55:17 +0530

    for testing

--- metamodel/extensions/run_pwsh_script_in_bash/templates/run_pwsh_script_in_bash.sh
+++ metamodel/extensions/run_pwsh_script_in_bash/templates/run_pwsh_script_in_bash.sh
@@ -16,6 +16,6 @@ ps_script_path=$(get_value_from_output_dictionary "ps_script_path")
 
 #region Execute the PowerShell script
 echo "Executing PowerShell script $ps_script_path"
-pwsh -f "$ps_script_path" "$(dirname "$0")" "$1"
+pwsh -File "$ps_script_path" "$(dirname "$0")" "$1"
 echo "Execution of PowerShell script $ps_script_path completed successfully."
 #endregion Execute the PowerShell script
